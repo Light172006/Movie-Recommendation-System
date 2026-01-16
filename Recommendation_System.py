e7a92a4541d418cecf1225e5f350a916137a1163
Light172006 <jashudebjit@gmail.com>
2026-01-17 01:52:27 +0530

    qbugs and clean repository

--- Recommendation_System.py
+++ Recommendation_System.py
@@ -1,65 +1,114 @@
+#import all the neccesory packages
+
 import pandas as pd
 import numpy as np 
-
+import ast
+from nltk.stem.porter import PorterStemmer
 from sklearn.metrics import pairwise_distances
+from sklearn.feature_extraction.text import CountVectorizer
 
-movies = pd.read_csv("D:\\DataSet\\Recomedation System\\ml-latest-small\\ml-latest-small\\movies.csv")
-rating = pd.read_csv("D:\\DataSet\\Recomedation System\\ml-latest-small\\ml-latest-small\\ratings.csv")
+#reading the movies and the credit file
+movies = pd.read_csv("D:\\DataSet\\Recomedation System\\archive\\tmdb_5000_movies.csv")
+credit = pd.read_csv("D:\\DataSet\\Recomedation System\\archive\\tmdb_5000_credits.csv")
 
-rating.drop(['timestamp'],axis=1,inplace=True)
+#removing all the unnecesory features form the dataframe
+movies = movies[['genres','id','keywords','overview','title']]
+credit = credit.drop(['movie_id'],axis=1)
 
-movies = movies.merge(rating.groupby('movieId').mean()[['rating']],on='movieId',how='inner')
+#merging movies and credit dataframe 
+movies = movies.merge(credit,how='inner',on='title')
 
-movies_user = rating.pivot(index='movieId',columns='userId',values='rating').reset_index(drop=True)
-movies_user.fillna(value=0,inplace=True)
+#droping all the na values
+movies.dropna(inplace=True)
 
-movies_sim = 1 - pairwise_distances(movies_user,metric='correlation',n_jobs=-1)
-movies_sim_df = pd.DataFrame(movies_sim)
-movies_sim_df.index = movies_user.index
-movies_sim_df.columns = movies_user.index
+#Converting the genre feature
+def clean_genre(x):
+    L = []
+    for i in ast.literal_eval(x):
+        L.append(i['name'])
+    return L
+#Converting the cast feature
+def clean_cast(x):
+    L = []
+    counter = 0
+    for i in ast.literal_eval(x):
+        if counter < 3:
+            counter += 1
+            L.append(i['name'])
+        else:
+            break
+    return L
 
+#Converting the crew feature
+def clean_crew(x):
+    for i in ast.literal_eval(x):
+        if i['job'] == 'Director':
+            return [i['name']]
+
+#Removing space between strings   
+def remove_space(temp):
+    l = []
+    for i in temp:
+        l.append(i.replace(' ',''))
+    return l
+
+def stem(text):
+    l = []
+    for i in text:
+        l.append(PorterStemmer().stem(i))
+    return l
+
+#cleaning
+movies['genres'] = movies['genres'].apply(clean_genre)
+movies['keywords'] = movies['keywords'].apply(clean_genre)
+movies['cast'] = movies['cast'].apply(clean_cast)
+movies['crew'] = movies['crew'].apply(clean_crew)
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+
+#droping all the null values
+movies.dropna(inplace=True)
+
+#Removing blankspace
+movies['cast'] = movies['cast'].apply(remove_space)
+movies['genres'] = movies['genres'].apply(remove_space)
+movies['crew'] = movies['crew'].apply(remove_space)
+movies['keywords'] = movies['keywords'].apply(remove_space)
+
+#Creating the tag feature
+movies['Tags'] = movies['overview'] + movies['keywords'] + movies['genres'] + movies['cast'] + movies['crew']
+
+#Storing all the neccesory feature into a new dataframe
+new_df = movies[['id','title','Tags']]
+new_df['Tags'] = new_df['Tags'].apply(stem)
+new_df['Tags'] = new_df['Tags'].apply(lambda x: ' '.join(x))
+
+#Vectorizing the tag
+cv = CountVectorizer(max_features = 5000,stop_words='english',lowercase=True)
+vector = cv.fit_transform(new_df['Tags']).toarray()
+vector = pd.DataFrame(vector,index=movies['id'],columns=cv.get_feature_names_out())
+
+#Calculating Similarity
+movies_sim = 1 - pairwise_distances(vector,metric='correlation',n_jobs=-1)
+movies_sim_df = pd.DataFrame(movies_sim,index=new_df.index,columns=new_df.index)
+
+#Getting top 5 similar movies
 def get_similer_movies(movieid,nTop = 5):
-    movieIdx = movies[movies.movieId == movieid].index
+    movieIdx = new_df[new_df.id == movieid].index
     if len(movieIdx) == 0:
         print('movie not found')
         return 
     else:
         movieIdx = movieIdx[0]
-    movies['similarity'] = movies_sim_df.iloc[movieIdx]
-    ntop = movies.sort_values(['similarity'],ascending=False)[0:nTop]
-    return ntop['title'].values , ntop['rating'].values , ntop['genres'].values ,ntop['Year'].values
+    new_df['similarity'] = movies_sim_df.iloc[movieIdx]
+    ntop = new_df.sort_values(['similarity'],ascending=False)[0:nTop]
+    return ntop['title'].values
 
-def low(x):
-    return x.lower()
+#returning all the movies name
+def movie_name():
+    return new_df.title.unique()
 
-#movies['title'] = movies['title'].apply(low)
-
-def check(x):
-    title = movies[['title']]
-    n = title.size
-    for i in range(n):
-        movie_name = title.iloc[i].values[0]
-        movie_name = movie_name.lower()
-        x = x.lower()
-        if x in movie_name:
-            movie_id = movies['movieId'].iloc[i]
-            return True,movie_id
-    return False,0
-
-def name(x):
-    name = x.split(' ')
-    year = name[-1]
-    name.pop()
-    return ' '.join(name)
-
-def get_year(x):
-    return x.split(' ')[-1]
-
-year = []
-for i in range(movies['title'].size):
-    year.append(get_year(movies['title'].iloc[i]))
-
-movies['Year'] = pd.DataFrame(year)
-
-movies['title'] = movies['title'].apply(name)
+#Return the movieId of a specific movie
+def get_id(name):
+    index = new_df[new_df.title == name].index[0]
+    return new_df.id.iloc[index]
 
